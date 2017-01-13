6e8b40b73302c8a6a5909a43ea26b6452028d858
Pooya Parsa <pooya@pi0.ir>
2017-01-13 12:36:06 +0330

    bak 0.1.0

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Controller = require('./lib/controller');
 const Model = require('./lib/mongoose/model');
 const Init = require('./lib/init');
 
+// We only expose core modules here
 module.exports = {
     Controller,
     Model,
