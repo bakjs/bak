b517fd312d7b0b5e44d843abab1867864e5fbaf2
Pooya Parsa <pooya@pi0.ir>
2018-05-28 02:16:00 +0430

    examples: add validate

--- examples/helloworld/controllers/api.js
+++ examples/helloworld/controllers/api.js
@@ -1,9 +1,22 @@
+import Joi from 'joi'
 import { Controller } from '../../..'
 
 export default class APIController extends Controller {
   init () {
     this.get('/hello/{name}', this.hello)
     this.get('/error', this.error)
+
+    this.get('/validate', this.validate, {
+      validate: {
+        query: {
+          name: Joi.string().required()
+        }
+      }
+    })
+  }
+
+  validate () {
+    return 'OK'
   }
 
   hello (request, h) {
