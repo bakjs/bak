04f35a97ae512ac8b3649e13c26bb5a73b6300a1
Pooya Parsa <pooya@pi0.ir>
2018-08-25 16:23:30 +0430

    fix(controller): remove validate.payload for GET and HEAD requests

--- examples/helloworld/controllers/api.js
+++ examples/helloworld/controllers/api.js
@@ -3,6 +3,14 @@ import { Controller } from '../../..'
 
 export default class APIController extends Controller {
   init () {
+    this.defaults = {
+      validate: {
+        payload: {
+          foo: Joi.string()
+        }
+      }
+    }
+
     this.get('/hello/{name}', this.hello)
     this.get('/error', this.error)
 
--- test/fixtures/basic/controllers/api.js
+++ test/fixtures/basic/controllers/api.js
@@ -1,7 +1,16 @@
+const Joi = require('joi')
 const { Controller } = require('../../../..')
 
 class APIController extends Controller {
   init () {
+    this.defaults = {
+      validate: {
+        payload: {
+          foo: Joi.string()
+        }
+      }
+    }
+
     this.get('/hello/{name}', this.hello)
   }
 
